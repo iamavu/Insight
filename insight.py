83ba7c1d8f2bd486c732aa838dab6c8ed9e424b6
Avantika <48346347+iamavu@users.noreply.github.com>
2021-02-10 17:05:44 +0530

    added insight

---
+++ insight.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#INSIGHT - The OSINT Tool
+
+__version__ = '0.0.1'
+
+from github import * 
+
+def banner():
+	cprint("""
+
+
+  _____           _       _     _   
+  \_   \_ __  ___(_) __ _| |__ | |_ 
+   / /\/ '_ \/ __| |/ _` | '_ \| __|
+/\/ /_ | | | \__ \ | (_| | | | | |_ 
+\____/ |_| |_|___/_|\__, |_| |_|\__|
+                    |___/           
+				> OSINT Tool
+				> Created by iamavu
+
+
+                    """, 'magenta')
+
+
+
+if __name__ == '__main__':
+
+	banner()
+	github()
+
+
+
