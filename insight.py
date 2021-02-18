16880391e269cb43bbb55420e8d54c2fa5cbe78f
Avantika <48346347+iamavu@users.noreply.github.com>
2021-02-18 11:40:35 +0530

    Updated insight
    Added notice of creation of folder of user in the current directory

--- insight.py
+++ insight.py
@@ -24,7 +24,10 @@ def banner():
 def insight():
     banner()
     userFetcher.setUser()
+    user = userFetcher.getUser()
     github()
+    cprint(f"Folder of {user} has been created in the current directory", 'cyan')
+    
     
 
 if __name__ == '__main__':
